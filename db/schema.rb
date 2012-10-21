b87076e42810950702f1408b7752b6768c77ecdb
Jim Ray <jimiray@gmail.com>
2012-10-21 07:03:46 -0600

    add caption to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121020192517) do
+ActiveRecord::Schema.define(:version => 20121021125052) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20121020192517) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "image"
+    t.text     "caption"
   end
 
   create_table "users", :force => true do |t|
